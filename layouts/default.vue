fd57d0b1dec2c0706d44f270529130073a5f88ec
isiljic <ivan.siljic.sspzo@gmail.com>
2024-09-16 21:13:33 +0200

    Project setup

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <ElContainer>
+        <Aside/>
+        <ElContainer>
+            <ElHeader>Header</ElHeader>
+            <ElMain>
+                <slot/>
+            </ElMain>
+            <ElFooter>Footer</ElFooter>
+        </ElContainer>
+    </ElContainer>
+</template>
+
+<style scoped>
+
+</style>
